9dd753fffa07afaa851f797bf86d79242e5cff5b
sgneha <sgneha05@gmail.com>
2020-06-13 15:58:32 +0100

    reverse alinked list

---
+++ DataStructure/LinkedList/reverse.java
@@ -0,0 +1,43 @@
+
+package DataStructure.LinkedList;
+
+class Reverse {
+    ListNode head;
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+        }
+        return prev;
+    }
+
+    void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Reverse list = new Reverse();
+        list.head = new ListNode(85);
+        list.head.next = new ListNode(15);
+        list.head.next.next = new ListNode(4);
+        list.head.next.next = new ListNode(4);
+        list.head.next.next.next = new ListNode(20);
+
+        System.out.println("Given Linked list");
+        list.printList(list.head);
+        ListNode newhead = list.reverseList(list.head);
+        System.out.println("");
+        System.out.println("Reversed linked list ");
+        list.printList(newhead);
+    }
+}
