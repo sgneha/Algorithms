cffa821c381861c08b67f5bcc12b0e37a617ac9c
sgneha <sgneha05@gmail.com>
2020-06-14 17:09:09 +0100

    refactored with structure of tree according to question example

--- DataStructure/Tree/MaxDepth.java
+++ DataStructure/Tree/MaxDepth.java
@@ -13,10 +13,11 @@ public class MaxDepth {
 
     public static void main(String[] args) {
         MaxDepth tree = new MaxDepth();
-        tree.root = new TreeNode(1);
-        tree.root.left = new TreeNode(2);
-        tree.root.right = new TreeNode(3);
-        tree.root.left.left = new TreeNode(4);
+        tree.root = new TreeNode(3);
+        tree.root.left = new TreeNode(9);
+        tree.root.right = new TreeNode(20);
+        tree.root.right.left = new TreeNode(15);
+        tree.root.right.right = new TreeNode(7);
         int result = tree.maxDepth(tree.root);
         System.out.println("Max Depth of given tree is " + result);
 
