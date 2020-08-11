7cc0dd090b610a742e43a598aba3a2c21ded4c03
sgneha <sgneha05@gmail.com>
2020-08-11 10:42:59 +0100

    finding cycle in linkedlist

---
+++ DataStructure/LinkedList/Cycle.java
@@ -0,0 +1,31 @@
+package DataStructure.LinkedList;
+
+public class Cycle {
+    ListNode head;
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        Cycle list = new Cycle();
+        list.head = new ListNode(85);
+        list.head.next = new ListNode(15);
+        list.head.next.next = new ListNode(4);
+        list.head.next.next.next = new ListNode(85);
+
+        System.out.println(list.hasCycle(list.head));
+    }
+
+}
